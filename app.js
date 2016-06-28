b6032bb66a4ce6df1a2846769ea7f391d1d32d29
Christian Castro <castroMBair@castroMBair.local>
2016-06-28 14:35:56 -0500

    Test3

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var bcrypt = require('bcrypt');
-var passport = require('passport');
+var passport = require('passport');//chris
 
 
 var routes = require('./routes/index');
